782808c5b2f6119c586288af0038d2a3f5f31f45
RaghavSingh <Raghav.Singh@lptpl.com>
2017-01-09 18:04:40 +0530

    update database adapter

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170109115223) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "city"
     t.string   "state"
